d9ce54e0478be602ff41f1ca586e234ca29fa192
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-09-11 17:45:37 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const number = Math.floor(Math.random() * 100 + 1);
 console.log(number);
 
-document.getElementById("Button").onclick = function Zadanie(liczba){
+document.getElementById("Button").onclick = function Zadanie(liczba) {
     liczba = document.getElementById("Input").value;
 
-    if(liczba > number){
+    if (liczba > number) {
         document.getElementById("Win").innerHTML = "Your number is too high";
     }
-    else if (liczba < number){
+    else if (liczba < number) {
         document.getElementById("Win").innerHTML = "Your number is too low";
     }
-    else if(liczba == number){
+    else if (liczba == number) {
         document.getElementById("Win").innerHTML = "Thats the number!";
     }
-    
-    
+
+
 }
